5917cc553b3b5577d75270c5358446f2090d1639
mugisha <mohamugi@gmail.com>
2024-05-21 13:29:10 +0300

    create hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import homeimage from "../images/homeimage.png"
+
+function Hero() {
+    return (
+        <div>
+            <h1>My exciting movie ranking site</h1>
+            <p>This is my enticing movie ranking site delivers an immersive and interactive platform that combines user-generated content and comprehensive film information. it aims to bring the excitement and thrill of the cinema to users screens, making movie exploration and discussion and engaging and enjoyable experience.</p>
+        <div>
+                
+        </div>
+            <img src={homeimage} alt="Yellowstone"></img>
+        </div>
+    )
+}
+
+export default Hero;
