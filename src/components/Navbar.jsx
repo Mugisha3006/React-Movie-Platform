923438059a4437cae517b53e5c2d8026cb597969
mugisha <mohamugi@gmail.com>
2024-05-22 00:00:05 +0300

    design the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 function Navbar() {
     return (
         <div>
-            <section>
-                <h1>M%M</h1>
-                <ul>
+            <section className=" bg-[#DA7B93] text-white flex justify-around items-center p-8">
+                <h1 className="text-5xl font-bold hover:cursor-pointer text-[#2F4454] ">M%M</h1>
+                <ul className="flex gap-8 text-xl">
                     <li>
                         <Link to="/">Home</Link>
                     </li>
@@ -17,14 +17,17 @@ function Navbar() {
                         <Link to="/">favourites</Link>
                     </li>
                     <li>
-                        <Link to="/">Aboutus</Link>
+                        <Link to="/">About-Us</Link>
                     </li>
                     
                 </ul>
 
-                <ul>
+                <ul className="flex justify-around gap-8 font-bold text-xl items-center">
                     <li>
                         <Link to="/">Sign In</Link>
+                    </li>
+                    <li className="text-white bg-[#2F4454] p-2 rounded-full ">
+                        <Link to="/">Sign Up</Link>
                     </li>
                 </ul>
             </section>
