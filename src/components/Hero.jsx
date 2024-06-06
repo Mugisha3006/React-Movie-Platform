edd9ee463eca5d83b7cd3f83313ed84f6bd0b016
mugisha <mohamugi@gmail.com>
2024-06-06 19:09:26 +0300

    add responsiveness to the different cards

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Hero(props) {
     return (
         <div className="m-auto w-[75%]">
-            <h1 className="font-sans text-white text-6xl p-8 text-center">{props.title }</h1>
+            <h1 className="font-sans text-white md:text-6xl text-4xl p-8 text-center">{props.title }</h1>
             <p className="text-white text-xl p-4 font-serif text-center">{props.definition }</p>
         <div>
                 
